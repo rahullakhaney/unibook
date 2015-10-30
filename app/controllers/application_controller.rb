cf22ce9705dc2e10f4f08ecb16fbd4db3fc3f1a9
Rahul Lakhaney <rahul.lakhaney@gmail.com>
2015-10-30 15:59:45 +0530

    Added normalize and fields to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:sign_up) << :username
   	devise_parameter_sanitizer.for(:account_update) << :username
+  	devise_parameter_sanitizer.for(:account_update) << :age
+  	devise_parameter_sanitizer.for(:account_update) << :gender
+  	devise_parameter_sanitizer.for(:account_update) << :bio
   end
 end
