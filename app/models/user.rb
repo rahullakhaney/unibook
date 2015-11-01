fa8bd7c0122a4b373f24a75695d9e2dfa4eb2161
Rahul Lakhaney <rahul.lakhaney@gmail.com>
2015-11-01 16:20:10 +0530

    Added friend types

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-         validates_presence_of :username
-         validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
-         validates_uniqueness_of :username
-         has_many :friendships, dependent: :destroy
-         has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  validates_presence_of :username
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates_uniqueness_of :username
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
 
-         def request_friendship(user_2)
-         	self.friendships.create(friend: user_2)
-         end
+  def request_friendship(user_2)
+  	self.friendships.create(friend: user_2)
+  end
+
+  def active_friends
+  	self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:friend)
+  end
+
+  def pending_friend_requests_from
+  	self.inverse_friendships.where(state: "pending")
+  end
 end
