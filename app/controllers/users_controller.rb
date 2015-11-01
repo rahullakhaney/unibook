64984c94b63a3ba7c5fd811d60bd6db6d00f8056
Rahul Lakhaney <rahul.lakhaney@gmail.com>
2015-11-01 13:27:08 +0530

    Added database seed file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   def show
   end
 
+  def index
+  	@users = User.all
+  end
+
   private
   def set_user
   	@user = User.find_by(username: params[:id])
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "RahulL1", email: "rahul1@rahul.com", password: "rahullll", password_confirmation: "rahullll")
+User.create(username: "RahulL2", email: "rahul2@rahul.com", password: "rahullll", password_confirmation: "rahullll")
+User.create(username: "RahulL3", email: "rahul3@rahul.com", password: "rahullll", password_confirmation: "rahullll")
+User.create(username: "RahulL4", email: "rahul4@rahul.com", password: "rahullll", password_confirmation: "rahullll")
+User.create(username: "RahulL5", email: "rahul5@rahul.com", password: "rahullll", password_confirmation: "rahullll")
+p "Test users created"
